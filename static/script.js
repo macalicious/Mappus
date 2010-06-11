3594dec66ec5bcfa7e797c7f2b9e13b77db0660e
Carsten Griesheimer <Jupiter@mb-jupiter.(none)>
2010-06-11 20:42:45 +0200

    --all

--- static/script.js
+++ static/script.js
@@ -254,7 +254,12 @@ $mapper.plugins.facebook = (function(){
        var t = '<fieldset>'
              + '<legend>Facebook</legend>'
              + '<section>Verbinde dich mit deinem Facebook-Konto und importiere die Daten.</section>'
-             + '<section class="last"><fb:login-button></section>' //<fb:intl>Connect with Facebook</fb:intl></fb:login-button>
+             + '<section class="last">'
+             + '<fb:if-is-verified>'
+             + 'Verbunden'
+             + '<fb:else><fb:login-button></fb:else>'
+             + '</fb:if-is-verified>'
+             + '</section>' //<fb:intl>Connect with Facebook</fb:intl></fb:login-button>
              + '</fieldset>';
        
        var obj = $j("#dialog1").append(t);
