38c0910a7925420794897eebc9d9a1027d84e302
Carsten Griesheimer <Jupiter@mb-jupiter.(none)>
2010-06-16 15:14:19 +0200

    --all

--- static/script.js
+++ static/script.js
@@ -270,6 +270,7 @@ var $mapper = (function(){
           r += '<hr />';
           r += '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
           r += '<section class="plugins">'
+          console.log("jaja: " + this.plugin_section);
           for(var plugin in this.plugin_sections){
             r += '<section id="' + plugin.name + 'plugin">';
             r += plugin.html;
