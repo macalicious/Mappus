f3b09b9afa241e6e722394d0e252a5eaba3bcafc
Carsten Griesheimer <Jupiter@mb-jupiter.(none)>
2010-06-22 10:59:31 +0200

    --all

--- sinatra.rb
+++ sinatra.rb
@@ -1,46 +1,47 @@
 require 'rubygems'
 require 'sinatra/base'
-require 'json'
-require 'HTTParty'
 
-class Google
-  include HTTParty
-  format :xml
-  
-  def self.geocode(ort)
-    @uri = URI.escape("http://maps.google.com/maps/geo?q=#{ort}&output=xml&sensor=false&key=ABQIAAAAIpjwufEkmHT623auy9W0EBQ1IfWKRoZ68hmR_kq9MWtN9i2BNRSR8hwLkR6TEX9GZDQxp6yQ8dv36Q")
-    @gResult =  get(@uri)
-    @cord = @gResult['kml']['Response']['Placemark']
-      @cord = @cord[0] if @cord.class == Array
-
-      if @cord
-        @cord['Point']['coordinates']   
-      else
-        geocode(ort)
-      end
-  end
-end
-
-
-class MyApp < Sinatra::Base
-  set :static, true
-  set :public, File.dirname(__FILE__) + '/static'
-  
-  get '/' do
-    erb :index
-  end
-  
-  get '/geocode' do
-    result = {}
-
-    params[:jupitermap].split(",").each do |item|
-      result[URI.escape(item).to_s] = Google.geocode(item) unless result.has_key?(item) 
-    end
-    
-    return result.to_json; 
-  end
-  
-end
+# require 'json'
+# require 'HTTParty'
+# 
+# class Google
+#   include HTTParty
+#   format :xml
+#   
+#   def self.geocode(ort)
+#     @uri = URI.escape("http://maps.google.com/maps/geo?q=#{ort}&output=xml&sensor=false&key=ABQIAAAAIpjwufEkmHT623auy9W0EBQ1IfWKRoZ68hmR_kq9MWtN9i2BNRSR8hwLkR6TEX9GZDQxp6yQ8dv36Q")
+#     @gResult =  get(@uri)
+#     @cord = @gResult['kml']['Response']['Placemark']
+#       @cord = @cord[0] if @cord.class == Array
+# 
+#       if @cord
+#         @cord['Point']['coordinates']   
+#       else
+#         geocode(ort)
+#       end
+#   end
+# end
+# 
+# 
+# class MyApp < Sinatra::Base
+#   set :static, true
+#   set :public, File.dirname(__FILE__) + '/static'
+#   
+#   get '/' do
+#     erb :index
+#   end
+#   
+#   get '/geocode' do
+#     result = {}
+# 
+#     params[:jupitermap].split(",").each do |item|
+#       result[URI.escape(item).to_s] = Google.geocode(item) unless result.has_key?(item) 
+#     end
+#     
+#     return result.to_json; 
+#   end
+#   
+# end
 
 
 
@@ -59,3 +60,14 @@ end
 #    }
 # )
 
+
+
+class MyApp < Sinatra::Base
+ 
+  
+  get '/' do
+    erb :index
+  end
+  
+  
+end
