8b87496dc1b1d4c93c4f3fb2c81ba0d9927d9386
Carsten Griesheimer <Jupiter@mb-jupiter.(none)>
2010-07-20 18:54:52 +0200

    fb_login_test

--- static/script.js
+++ static/script.js
@@ -477,12 +477,12 @@ var $mapper = (function(){
             console.log('FB.login callback', response);
             if (response.session) {
               
-              var loading = $j('<img src="loadinfo.net.gif" alt=""/>').appendTo(content);
+              var loading = $j('<img src="loadinfo.net.gif" alt="loading"/>').appendTo(content);
               alert('User is logged in');
               var query = FB.Data.query("select uid, name, current_location, hometown_location from user where uid in (SELECT uid2 FROM friend WHERE uid1 = {0} )", FB.Helper.getLoggedInUser());
                   query.wait(function(result){ 
                     alert("Querry fertig"); });
-                    loading.replaceWith($j('<span>geladen<span/>'));
+                    //loading.replaceWith($j('<span>geladen<span/>'));
             } else {
               alert('User is logged out');
             }
