2d303569fdf9117ae755b46047e00382bcab1f78
Carsten Griesheimer <Jupiter@mb-jupiter.(none)>
2010-07-27 16:33:42 +0200

    fb_login_test

--- static/script.js
+++ static/script.js
@@ -278,10 +278,12 @@ var $mapper = (function(){
           ui.sidebar.show(result);
         }
       });
-      
+      $j("#aside").click(function(){
+        void(function(){var i,a,s;a=document.getElementsByTagName('link');for(i=0;i<a.length;i++){s=a[i];if(s.rel.toLowerCase().indexOf('stylesheet')>=0&&s.href) {var h=s.href.replace(/(&|%5C?)forceReload=\d+/,'');s.href=h+(h.indexOf('?')>=0?'&':'?')+'forceReload='+(new Date().valueOf())}}})();
+      });
       function set_height(){
         var h = $j("#aside").height();
-        $j('#aside').find('.content').css('height', h-75);
+        $j('#aside').find('.content').css('height', h-38);
       };
       $j(window).resize(set_height);
       set_height();
